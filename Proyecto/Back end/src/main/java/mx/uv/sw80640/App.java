80e4a82503f0aa7f77454bec2e4fc05e45a4a7e8
Emanoxxx <emanohalowar@hotmail.com>
2020-12-14 09:49:56 -0600

    Back end final

--- Proyecto/Back end/src/main/java/mx/uv/sw80640/App.java
+++ Proyecto/Back end/src/main/java/mx/uv/sw80640/App.java
@@ -33,8 +33,10 @@ public class App
 
     public static void main( String[] args )
     {   
-        port(2022);
+        port(getHerokuAssignedPort());
         get("/UserPage", (rq, rs) -> new ModelAndView(getCuentosUser(""+rq.queryParams("U")), "UserPage"), new JadeTemplateEngine());
+        get("/EditPage", (rq, rs) -> new ModelAndView(getCuentosUser(""+rq.queryParams("U")), "EditPage"), new JadeTemplateEngine());
+        get("/Search", (rq, rs) -> new ModelAndView(getCuentosSearch(""+rq.queryParams("S")), "Search"), new JadeTemplateEngine());
         get("/ReadPage", (rq, rs) -> new ModelAndView(getCuentoc(""+rq.queryParams("U"),""+rq.queryParams("N")), "ReadPage"), new JadeTemplateEngine());
         /*get("/UserPage",(request,response)->{
             String user= ""+request.queryParams("Nuser");
@@ -62,6 +64,14 @@ public class App
             System.out.println(x+" "+usuario.getId());
             return x;
         });
+        post("/top5",(req,res)->{
+            try{
+            String cuentos =DAO.getTop();
+            return cuentos;
+            }catch(Exception e){
+                return e.getMessage();
+            }
+        });
         post("/NCuentos",(req,res)->{
             try{
             JsonParser parser = new JsonParser();
@@ -70,14 +80,6 @@ public class App
             String u= ""+peticion.get("Username");
             System.out.println(u);
             return DAO.getNCuentos(u);
-            }catch(Exception e){
-                return e.getMessage();
-            }
-        });
-        post("/top5",(req,res)->{
-            try{
-            List<Cuento> cuentos =DAO.getTop();
-            return cuentos;
             }catch(Exception e){
                 return e.getMessage();
             }
@@ -93,6 +95,33 @@ public class App
             String g= ""+peticion.get("genero");
             System.out.println(u);
             return DAO.agregarCuento(n,u,c,g);
+            }catch(Exception e){
+                return e.getMessage();
+            }
+        });
+        post("/UpdCuento",(req,res)->{
+            try{
+            JsonParser parser = new JsonParser();
+            JsonElement arbol = parser.parse(req.body());
+            JsonObject peticion =arbol.getAsJsonObject();
+            String n= ""+peticion.get("nombre");
+            String u= ""+peticion.get("user");
+            String c= ""+peticion.get("contenido");
+            String g= ""+peticion.get("genero");
+            System.out.println(u);
+            return DAO.updateCuento(n,u,c,g);
+            }catch(Exception e){
+                return e.getMessage();
+            }
+        });
+        post("/delCuento",(req,res)->{
+            try{
+            JsonParser parser = new JsonParser();
+            JsonElement arbol = parser.parse(req.body());
+            JsonObject peticion =arbol.getAsJsonObject();
+            String n= ""+peticion.get("nombre");
+            String u= ""+peticion.get("user");
+            return DAO.delCuento(n,u);
             }catch(Exception e){
                 return e.getMessage();
             }
@@ -160,8 +189,54 @@ public class App
             }
               
         });
+        post("/getContenido", (req, res) ->{
+            try{
+            JsonParser parser = new JsonParser();
+            JsonElement arbol = parser.parse(req.body());
+            JsonObject peticion =arbol.getAsJsonObject();
+            String name= ""+peticion.get("nombre");
+            String user= ""+peticion.get("Usuario");
+            return getContenido(name, user);
+            }catch(Exception e){
+                   return e.getMessage();
+            }
+              
+        });
+        
+        post("/getGenero", (req, res) ->{
+            try{
+            JsonParser parser = new JsonParser();
+            JsonElement arbol = parser.parse(req.body());
+            JsonObject peticion =arbol.getAsJsonObject();
+            String name= ""+peticion.get("nombre");
+            String user= ""+peticion.get("Usuario");
+            return getGenero(name, user);
+            }catch(Exception e){
+                   return e.getMessage();
+            }
+              
+        });
         
         
+        
+}
+public static String getGenero(String nombre, String user){
+    String x="";
+    x=DAO.getGenero(nombre,user);
+    return x;
+}
+public static String getContenido(String nombre, String user){
+    String x="";
+    Cuento cuento =DAO.getCuentoContenido(nombre, user);
+    x=cuento.getContent();
+    return x;
+}
+static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
 }
 public static boolean validar(String nombre, String pass){
     if(DAO.getvalidar(nombre, pass)){
@@ -187,6 +262,12 @@ public static Map<String, String> getCuentoc(String user,String nombre){
     map.put("Nombre", nombre);
     map.put("Usuario", user);
     map.put("Contenido",cuento.getContent());
+    return map;
+}
+public static Map<String, Object> getCuentosSearch(String busqueda){
+    Map<String, Object> map = new HashMap<>();
+    map.put("Nombre", ("Busqueda="+busqueda));
+    map.put("Cuentos",DAO.getCuentosSearch(busqueda));
     return map;
 }
 }
--- Proyecto/Back end/src/main/java/mx/uv/sw80640/DAO.java
+++ Proyecto/Back end/src/main/java/mx/uv/sw80640/DAO.java
@@ -104,23 +104,39 @@ public class DAO {
         }
         return x;
     }
-    //select * from Cuento order by vistas desc;
-    public static List<Cuento> getTop(){
-        String x="No creado";
+    //getCuentosSearch
+    public static List<Cuento> getCuentosSearch(String busqueda){
         java.sql.Statement stm = null;
         Connection con=null;
         ResultSet rs =null;
         List<Cuento> cuentos = new ArrayList<Cuento>();
-        //Cuento c=new Cuento(nombre,user);
         String contenido="No se encontro o hubo un error.";
         con = conn.getConexion();
         try {
-            String sql="select * from Cuento order by vistas desc";
+            String sql="Select nombre,usuario from Cuento where usuario='"+busqueda+"'";
             stm=con.createStatement();
             rs=stm.executeQuery(sql);
            
            int i=0;
-            while(rs.next() & i<5){
+            while(rs.next()){
+                i++;
+                cuentos.add(new Cuento(rs.getString("nombre"),rs.getString("usuario")));
+            }
+            //Titulo
+            sql="Select nombre,usuario from Cuento where nombre='"+busqueda+"'";
+            rs=stm.executeQuery(sql);
+           
+            i=0;
+            while(rs.next()){
+                i++;
+                cuentos.add(new Cuento(rs.getString("nombre"),rs.getString("usuario")));
+            }
+            //genero
+            sql="Select nombre,usuario from Cuento where genero='"+busqueda+"'";
+            rs=stm.executeQuery(sql);
+           
+            i=0;
+            while(rs.next()){
                 i++;
                 cuentos.add(new Cuento(rs.getString("nombre"),rs.getString("usuario")));
             }
@@ -209,8 +225,59 @@ public class DAO {
         return cuentos;
     }
     //
-    public static Cuento getCuento(String nombre,String user){
+    public static String getTop(){
         String x="No creado";
+        java.sql.Statement stm = null;
+        Connection con=null;
+        ResultSet rs =null;
+        String cuentos = "";
+        //Cuento c=new Cuento(nombre,user);
+        String contenido="No se encontro o hubo un error.";
+        con = conn.getConexion();
+        try {
+            String sql="select * from Cuento order by vistas desc";
+            stm=con.createStatement();
+            rs=stm.executeQuery(sql);
+           
+           int i=0;
+            while(rs.next() & i<5){
+                i++;
+                cuentos=cuentos + "Cuento-"+(i)+": N("+ rs.getString("nombre")+")U("+rs.getString("usuario")+")";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(rs!=null){
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                rs=null;
+            }
+            if(stm!=null){
+                try {
+                    stm.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                stm=null;
+            }
+            if(con!=null){
+                try {
+                    con.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                con=null;
+            }
+        }
+        
+        
+        return cuentos;
+    }
+    //getGenero
+    public static String getGenero(String nombre,String user){
         java.sql.Statement stm = null;
         Connection con=null;
         ResultSet rs =null;
@@ -218,7 +285,57 @@ public class DAO {
         String contenido="No se encontro o hubo un error.";
         con = conn.getConexion();
         try {
-            String sql="Select contenido from Cuento where nombre='"+nombre+"'and usuario='"+user+"'";
+            String sql="Select genero from Cuento where nombre="+nombre+"and usuario="+user+"";
+            stm=con.createStatement();
+            rs=stm.executeQuery(sql);
+            while(rs.next()){
+                contenido= rs.getString("genero");
+            }
+            stm.execute(sql);
+            //update Cuento set vistas=vistas+1 where nombre="El perro aterrado" and usuario="emanoxxx";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(rs!=null){
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                rs=null;
+            }
+            if(stm!=null){
+                try {
+                    stm.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                stm=null;
+            }
+            if(con!=null){
+                try {
+                    con.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                con=null;
+            }
+        }
+        return contenido;
+    }
+    
+    //getCuentoContenido
+    public static Cuento getCuentoContenido(String nombre,String user){
+        String x="No creado";
+        java.sql.Statement stm = null;
+        Connection con=null;
+        ResultSet rs =null;
+        List<Usuarios> resultado =new ArrayList<>();
+        String contenido="No se encontro o hubo un error."+nombre+"nn"+user;
+        con = conn.getConexion();
+        try {
+            String sql="Select contenido from Cuento where nombre="+nombre+"and usuario="+user+"";
+            contenido=sql;
             stm=con.createStatement();
             rs=stm.executeQuery(sql);
            
@@ -227,6 +344,9 @@ public class DAO {
                 i++;
                 contenido= rs.getString("contenido");
             }
+            sql="update Cuento set vistas=vistas+1 where nombre='"+nombre+"'and usuario='"+user+"'";
+            stm.execute(sql);
+            //update Cuento set vistas=vistas+1 where nombre="El perro aterrado" and usuario="emanoxxx";
         } catch (Exception e) {
             e.printStackTrace();
         }finally{
@@ -259,6 +379,111 @@ public class DAO {
         Cuento c=new Cuento(nombre,user,contenido);
         return c;
     }
+    //
+    public static Cuento getCuento(String nombre,String user){
+        String x="No creado";
+        java.sql.Statement stm = null;
+        Connection con=null;
+        ResultSet rs =null;
+        List<Usuarios> resultado =new ArrayList<>();
+        String contenido="No se encontro o hubo un error."+nombre+"nn"+user;
+        con = conn.getConexion();
+        try {
+            String sql="Select contenido from Cuento where nombre='"+nombre+"'and usuario='"+user+"'";
+            contenido=sql;
+            stm=con.createStatement();
+            rs=stm.executeQuery(sql);
+           
+           int i=0;
+            while(rs.next()){
+                i++;
+                contenido= rs.getString("contenido");
+            }
+            sql="update Cuento set vistas=vistas+1 where nombre='"+nombre+"'and usuario='"+user+"'";
+            stm.execute(sql);
+            //update Cuento set vistas=vistas+1 where nombre="El perro aterrado" and usuario="emanoxxx";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(rs!=null){
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                rs=null;
+            }
+            if(stm!=null){
+                try {
+                    stm.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                stm=null;
+            }
+            if(con!=null){
+                try {
+                    con.close();
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+                con=null;
+            }
+        }
+        
+        Cuento c=new Cuento(nombre,user,contenido);
+        return c;
+    }//updateCuento
+    public static String updateCuento(String nombre,String username,String c,String gen){
+        String r="NO";
+        java.sql.Statement stm = null;
+        Connection con=null;
+        ResultSet rs =null;
+        List<Usuarios> resultado =new ArrayList<>();
+        con = conn.getConexion();
+        try {
+            //String sql="Update Usuario set nombre="+nombre+" where id="+username;
+            String sql="Update Cuento set nombre="+nombre+",contenido="+c+",genero="+gen+" where nombre="+nombre+"and usuario="+username+"";
+            stm=con.createStatement();
+            stm.execute(sql);
+            r="SI";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(rs!=null){
+                try {rs.close();} catch (Exception e) {}rs=null;}
+            if(stm!=null){
+                try {stm.close();} catch (Exception e) {}stm=null;}
+            if(con!=null){
+                try {con.close();} catch (Exception e) {}con=null;System.out.println("Conexion cerrada");}}
+        return r;
+    }
+    public static String delCuento(String nombre,String username){
+        String r="NO";
+        java.sql.Statement stm = null;
+        Connection con=null;
+        ResultSet rs =null;
+        List<Usuarios> resultado =new ArrayList<>();
+        con = conn.getConexion();
+        try {
+            //String sql="Update Usuario set nombre="+nombre+" where id="+username;
+            String sql="delete from Cuento where nombre="+nombre+"and usuario="+username+"";
+            stm=con.createStatement();
+            stm.execute(sql);
+            r="SI";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(rs!=null){
+                try {rs.close();} catch (Exception e) {}rs=null;}
+            if(stm!=null){
+                try {stm.close();} catch (Exception e) {}stm=null;}
+            if(con!=null){
+                try {con.close();} catch (Exception e) {}con=null;System.out.println("Conexion cerrada");}}
+        return r;
+    }
+    //
+
     public static String agregarCuento(String nombre,String username,String c,String gen){
         String r="NO";
         java.sql.Statement stm = null;
